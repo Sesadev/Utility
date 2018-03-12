983dce74e4271c69dcd59d1b7a1c4aaf9d2b5de8
Jitendra Kumar Pradhan <jiten@bsetxt.com>
2018-03-12 17:21:49 +0530

    Email validations

--- Utility/YLog.swift
+++ Utility/YLog.swift
@@ -10,7 +10,7 @@ import Foundation
 
  public class YLog {
     private var isDebug: Bool!
-    
+    //jiten saved
     //2.
     public init() {
         self.isDebug = false
@@ -29,5 +29,11 @@ import Foundation
         } else {
             //Do any stuff for production mode
         }
+    }
+   public func isValidEmail(testStr:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: testStr)
     }
 }
